a1d871075ef49d1bdf20c9cfcfe0f1f9cdc68658
vihpol <vihaanp2007@gmail.com>
2026-07-08 15:02:30 -0700

    Allow VM frontend origin

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "http://192.168.1.174:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
